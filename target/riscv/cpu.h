ed10f5f4a7fb59a35641a7e05d8cb051f0a6bf48
LIU Zhiwei <zhiwei_liu@c-sky.com>
2021-04-02 14:09:45 +0800

    hw/intc: Add CLIC device
    The Core-Local Interrupt Controller (CLIC) provides low-latency,
    vectored, pre-emptive interrupts for RISC-V systems.
    
    The CLIC also supports a new Selective Hardware Vectoring feature
    that allow users to optimize each interrupt for either faster
    response or smaller code size.
    
    Signed-off-by: LIU Zhiwei <zhiwei_liu@c-sky.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -42,6 +42,7 @@
 #define RISCV_CPU_TYPE_SUFFIX "-" TYPE_RISCV_CPU
 #define RISCV_CPU_TYPE_NAME(name) (name RISCV_CPU_TYPE_SUFFIX)
 #define CPU_RESOLVING_TYPE TYPE_RISCV_CPU
+#define CPU_INTERRUPT_CLIC CPU_INTERRUPT_TGT_EXT_0
 
 #define TYPE_RISCV_CPU_ANY              RISCV_CPU_TYPE_NAME("any")
 #define TYPE_RISCV_CPU_BASE32           RISCV_CPU_TYPE_NAME("rv32")
@@ -378,6 +379,7 @@ struct CPUArchState {
     bool vstime_irq;
 
     void *clic;       /* clic interrupt controller */
+    uint64_t exccode; /* clic irq encode */
 
     hwaddr kernel_addr;
     hwaddr fdt_addr;
