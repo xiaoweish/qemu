ed0c4c72f10142a13038a4636a64f715bad74f1a
LIU Zhiwei <zhiwei_liu@c-sky.com>
2021-04-02 15:21:24 +0800

    target/riscv: Update CSR mclicbase in CLIC mode
    The machine mode mclicbase CSR is an XLEN-bit read-only register providing
    the base address of CLIC memory mapped registers.
    
    Signed-off-by: LIU Zhiwei <zhiwei_liu@c-sky.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -208,6 +208,7 @@ struct CPUArchState {
     uint64_t miclaim;
     uint64_t mintstatus; /* clic-spec */
     target_ulong mintthresh; /* clic-spec */
+    target_ulong mclicbase; /* clic-spec */
 
     uint64_t mie;
     uint64_t mideleg;
